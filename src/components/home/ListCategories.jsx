9136351c23456621ee66692279984c9e966a65f1
Juan Marquina <juanantoniomarquina@gmail.com>
2022-10-14 21:48:09 -0500

    Se agrego a la lista categoria el key

--- src/components/home/ListCategories.jsx
+++ src/components/home/ListCategories.jsx
@@ -7,11 +7,11 @@ const ListCategories = ({categories}) => {
                     {categories.map( (element) => {
                                 const { link_foto } = element;
                                 return (
-                                    <div className="col-lg-2 col-md-4 col-sm-6 col-6 mt-1 mb-1">
-                                    <div className="card">
-                                        <img src={link_foto} className="card-img-top img__categorie" alt="..." />
+                                    <div key={element.id} className="col-lg-2 col-md-4 col-sm-6 col-6 mt-1 mb-1">
+                                        <div className="card">
+                                            <img src={link_foto} className="card-img-top img__categorie" alt="..." />
+                                        </div>
                                     </div>
-                                </div>
                             );
                     })}     
                 </div>
