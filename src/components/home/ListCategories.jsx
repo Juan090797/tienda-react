95d7cf3a8f732a134e14494b42bc8293fa452984
Juan Marquina <juanantoniomarquina@gmail.com>
2022-10-14 21:54:58 -0500

    Se #3

--- src/components/home/ListCategories.jsx
+++ src/components/home/ListCategories.jsx
@@ -9,7 +9,7 @@ const ListCategories = ({categories}) => {
                                 return (
                                     <div key={element.id} className="col-lg-2 col-md-4 col-sm-6 col-6 mt-1 mb-1">
                                         <div className="card">
-                                            <img src={link_foto} className="card-img-top img__categorie" alt="..." />
+                                            <img src={link_foto} className="card-img-top img__categorie" alt=".." />
                                         </div>
                                     </div>
                             );
