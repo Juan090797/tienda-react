63797071319588f0e27b3da6737570e76e69b2c6
Juan Marquina <juanantoniomarquina@gmail.com>
2022-10-15 08:19:31 -0500

    actualizacionv10

--- src/pages/Contacto.jsx
+++ src/pages/Contacto.jsx
@@ -5,7 +5,6 @@ const Contacto = () => {
                 <div className="contact-image">
                     <img src="https://image.ibb.co/kUagtU/rocket_contact.png" alt="rocket_contact" />
                 </div>
-                <form >
                     <h3>Contactanos</h3>
                     <div className="row">
                         <div className="col-md-6">
@@ -19,7 +18,7 @@ const Contacto = () => {
                                 <input type="text" name="txtPhone" className="form-control" placeholder="Tu celular *"  />
                             </div>
                             <div className="form-group mb-4">
-                                <a type="submit" name="btnSubmit" className="btnContact btnContactSubmit" href="/">Enviar</a>
+                                <a name="btnSubmit" className="btnContact btnContactSubmit" href="/">Enviar</a>
                             </div>
                         </div>
                         <div className="col-md-6 mb-4">
@@ -28,7 +27,6 @@ const Contacto = () => {
                             </div>
                         </div>
                     </div>
-                </form>
             </div>
         </>
      );
